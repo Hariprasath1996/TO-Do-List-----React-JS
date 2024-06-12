e39079a187a4aa748a6b4e25ed005f6530ab7167
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-12 21:09:12 +0530

    create elements

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,16 @@ function App() {
   return (
     <>
       <div className='TO-Do-container'>
+        <h1>To-Do-List</h1>
         <div className='input-container'>
-          <input type="text" />
-          <button>Add</button>
+          <input className='input' type="text" />
+          <button className='add'><b>ADD</b></button>
         </div>
         <div className='list-container'>
           <ul className='Un-order'>
             <li>Book</li>
-            <li>Glass</li>
-            <li>Box</li>
+            <button className='edit'><b>Edit</b></button>
+            <button className='delete'><b>Delete</b></button>
           </ul>
         </div>
       </div>
