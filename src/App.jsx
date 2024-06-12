9db70c50c2c57bcedf934e3fd73889f5a7c8b178
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-12 22:42:36 +0530

    add items logic created

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import { useState } from 'react'
-
-
+import { FaEdit } from "react-icons/fa";
+import { FaDeleteLeft } from "react-icons/fa6";
 
 function App() {
-
+  const [items, setItems] = useState([]);
+  const addItem = (itemName) => {
+    setItems([...items, { id: Date.now(), name: itemName }])
+  }
   return (
     <>
       <div className='TO-Do-container'>
         <h1>To-Do-List</h1>
         <div className='input-container'>
           <input className='input' type="text" />
-          <button className='add'><b>ADD</b></button>
+          <button className='add' onClick={() => addItem(prompt("type here"))}><b>ADD</b></button>
         </div>
-        <div className='list-container'>
+        <div className='list-container' >
           <ul className='Un-order'>
-            <li>Book</li>
-            <button className='edit'><b>Edit</b></button>
-            <button className='delete'><b>Delete</b></button>
+            <div className='list'>
+              {items.map((item => (
+                <div className=''>
+                  <li key={item.id}>{item.name}</li>
+                  <button className='Edit'><FaEdit />
+                  </button>
+                  <button className='Delete'>
+                  <FaDeleteLeft />
+                  </button>
+                </div>
+              )))}
+            </div>
           </ul>
         </div>
       </div>
