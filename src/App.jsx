39402442a45d87b3bbf7805d8f92333b38b0f02e
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-12 20:37:45 +0530

    create elements

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,19 @@ function App() {
 
   return (
     <>
-
+      <div className='TO-Do-container'>
+        <div className='input-container'>
+          <input type="text" />
+          <button>Add</button>
+        </div>
+        <div className='list-container'>
+          <ul className='Un-order'>
+            <li>Book</li>
+            <li>Glass</li>
+            <li>Box</li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
