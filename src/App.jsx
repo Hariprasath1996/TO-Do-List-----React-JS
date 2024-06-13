05be1b2ab7cf12cbaf9981e816e0c2379642b455
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-13 08:47:10 +0530

    created add eent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import { FaEdit } from "react-icons/fa";
-import { FaDeleteLeft } from "react-icons/fa6";
+
 
 function App() {
   const [items, setItems] = useState([]);
@@ -13,18 +12,17 @@ function App() {
         <h1>To-Do-List</h1>
         <div className='input-container'>
           <input className='input' type="text" />
-          <button className='add' onClick={() => addItem(prompt("type here"))}><b>ADD</b></button>
+          <button className='btn-add' onClick={() => addItem(prompt("type here"))}><b>ADD</b></button>
         </div>
         <div className='list-container' >
           <ul className='Un-order'>
             <div className='list'>
               {items.map((item => (
-                <div className=''>
-                  <li key={item.id}>{item.name}</li>
-                  <button className='Edit'><FaEdit />
+                <div className='list-items' key={item.id}>{item.name}
+                  <button className='btn-Edit'>Edit
                   </button>
-                  <button className='Delete'>
-                  <FaDeleteLeft />
+                  <button className='btn-Delete'>
+                Delete
                   </button>
                 </div>
               )))}
